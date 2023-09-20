f14faa413ba93cd5c3e92fcfc640ebc05ffd2ce7
YAVILES <yonathanaviles123@gmail.com>
2023-09-20 14:50:39 -0400

    Update optionsRutas.php

--- packages/precliente/paso_venta/views/optionsRutas.php
+++ packages/precliente/paso_venta/views/optionsRutas.php
@@ -5,6 +5,7 @@ $precliente = $_POST['precliente'];
 $pasoventa = new Pasoventa;
 $sub_rutas = $pasoventa->get_rutas($precliente);
 
+echo '<option value="">Seleccione</option>';
 foreach ($sub_rutas as  $datos) {
 	echo '<option value="'.$datos[0].'">'.$datos[1].'</option>';
 }
