a4ab464214446089350ee5a74f882bab25c985b5
YAVILES <yonathanaviles123@gmail.com>
2024-03-31 23:32:43 -0400

    Update inicio.php

--- packages_mant/inicio/modelo/inicio.php
+++ packages_mant/inicio/modelo/inicio.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 define("SPECIALCONSTANT", true);
 
 include_once "../../../funciones/funciones.php";
