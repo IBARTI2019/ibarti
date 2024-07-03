fa43d4cda6c786b6e5227e3c7a5a6fee07eda01a
YAVILES <yonathanaviles123@gmail.com>
2024-07-03 12:56:40 -0400

    Configuracion de rango de confirmaciones

---
+++ autentificacion/cont_confirmaciones.php
@@ -0,0 +1,48 @@
+	<table width="80%" align="center">
+   <tr valign="top">                    
+     <td height="23" colspan="2" class="etiqueta_title" align="center">CONTROL DE CONFIRMACIONES</td>
+   </tr>
+   <tr><td height="8" colspan="2" align="center"><hr></td></tr>			 
+   <tr>  
+     <td class="etiqueta" width="40%">Rango minimo de minutos para confirmacion de asistencia: </td>
+     <td width="60%">
+      <input type="number" required="required" min="1" name="min_confirm" value="<?php echo $min_confirm ;?>">
+    </td>
+   </tr>
+   <tr>  
+     <td class="etiqueta" width="40%">Rango maximo de minutos para confirmacion de asistencia: </td>
+     <td width="60%">
+      <input type="number" required="required" min="1" name="max_confirm" value="<?php echo $max_confirm ;?>">
+    </td>
+   </tr>
+   <tr>  
+     <td class="etiqueta" width="40%">Rango minimo de minutos para confirmacion en transporte: </td>
+     <td width="60%">
+      <input type="number" required="required" min="1" name="min_in_transport" value="<?php echo $min_in_transport ;?>">
+    </td>
+   </tr>
+   <tr>  
+     <td class="etiqueta" width="40%">Rango maximo de minutos para confirmacion en transporte: </td>
+     <td width="60%">
+      <input type="number" required="required" min="1" name="max_in_transport" value="<?php echo $max_in_transport ;?>">
+    </td>
+   </tr>
+   <tr><td height="4" colspan="2" align="center"><hr></td></tr>			  
+   <tr>      
+   </table>
+   <div align="center"><span class="art-button-wrapper">
+    <span class="art-button-l"> </span>
+    <span class="art-button-r"> </span>
+    <input type="submit" name="salvar"  id="salvar" value="Guardar" class="readon art-button" />	
+  </span>&nbsp;
+  <span class="art-button-wrapper">
+    <span class="art-button-l"> </span>
+    <span class="art-button-r"> </span>
+    <input type="reset" id="limpiar" value="Restablecer" class="readon art-button" />	
+  </span>&nbsp;
+  <span class="art-button-wrapper">
+    <span class="art-button-l"> </span>
+    <span class="art-button-r"> </span>
+    <input type="button" id="volver" value="Volver" onClick="history.back(-1);" class="readon art-button" />	
+  </span>           
+</div> 
--- autentificacion/cont_sistema.php
+++ autentificacion/cont_sistema.php
@@ -32,7 +32,8 @@ control.cl_campo_04_act, control.cl_campo_04_desc,
 control.cod_turno_dl, turno.descripcion AS turno_dl ,
 control.control_arma_linea AS cod_ar_linea, prod_lineas.descripcion AS ar_linea,
 control.control_uniforme_linea AS cod_uniforme_linea, pl.descripcion AS uniforme_linea,
-control.url_doc, control.rop_meses,control.dias_nov_notif,control.min_nov_notif, control.porc_min_aprob_encuesta_preing
+control.url_doc, control.rop_meses,control.dias_nov_notif,control.min_nov_notif, control.porc_min_aprob_encuesta_preing,
+control.min_confirm, control.max_confirm, control.min_in_transport, control.max_in_transport
 FROM control, paises, clientes, ficha_status,
 conceptos, conceptos AS r, conceptos AS cest, conceptos AS hora_ex_d, conceptos AS hora_ex_n,
 roles, cargos, turno, prod_lineas, prod_lineas pl
@@ -104,7 +105,10 @@ $rop_meses       = $result['rop_meses'];
 $dias_nov_notif         = $result['dias_nov_notif'];
 $min_nov_notif       = $result['min_nov_notif'];
 $porc_min_aprob_encuesta_preing       = $result['porc_min_aprob_encuesta_preing'];
-
+$min_confirm       = $result['min_confirm'];
+$max_confirm       = $result['max_confirm'];
+$min_in_transport       = $result['min_in_transport'];
+$max_in_transport       = $result['max_in_transport'];
 ?>
 <table width="80%" align="center">
   <tr valign="top">
