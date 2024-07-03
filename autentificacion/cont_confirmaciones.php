ddffc8f465ca3ecdf2c79841f664ca275c0ba8cb
YAVILES <yonathanaviles123@gmail.com>
2024-07-03 13:34:57 -0400

    Update cont_confirmaciones.php

--- autentificacion/cont_confirmaciones.php
+++ autentificacion/cont_confirmaciones.php
@@ -4,25 +4,25 @@
    </tr>
    <tr><td height="8" colspan="2" align="center"><hr></td></tr>			 
    <tr>  
-     <td class="etiqueta" width="40%">Rango minimo de minutos para confirmacion de asistencia: </td>
+     <td class="etiqueta" width="40%">Minimo de minutos para confirmacion de asistencia: </td>
      <td width="60%">
       <input type="number" required="required" min="1" name="min_confirm" value="<?php echo $min_confirm;?>">
     </td>
    </tr>
    <tr>  
-     <td class="etiqueta" width="40%">Rango maximo de minutos para confirmacion de asistencia: </td>
+     <td class="etiqueta" width="40%">Maximo de minutos para confirmacion de asistencia: </td>
      <td width="60%">
       <input type="number" required="required" min="1" name="max_confirm" value="<?php echo $max_confirm;?>">
     </td>
    </tr>
    <tr>  
-     <td class="etiqueta" width="40%">Rango minimo de minutos para confirmacion en transporte: </td>
+     <td class="etiqueta" width="40%">Minimo de minutos para confirmacion en transporte: </td>
      <td width="60%">
       <input type="number" required="required" min="1" name="min_in_transport" value="<?php echo $min_in_transport;?>">
     </td>
    </tr>
    <tr>  
-     <td class="etiqueta" width="40%">Rango maximo de minutos para confirmacion en transporte: </td>
+     <td class="etiqueta" width="40%">Maximo de minutos para confirmacion en transporte: </td>
      <td width="60%">
       <input type="number" required="required" min="1" name="max_in_transport" value="<?php echo $max_in_transport;?>">
     </td>
