2b5c3875777cf6244c040ed22b290e1297efd995
YAVILES <yonathanaviles123@gmail.com>
2024-07-09 21:40:10 -0400

    Adaptaciones pantalla de confirmaciones de asist

--- packages/planif/planif_confirmaciones/views/Add_planif.php
+++ packages/planif/planif_confirmaciones/views/Add_planif.php
@@ -11,33 +11,24 @@ $result  =  $confirmaciones->get_planif($ficha, $cliente, $ubicacion);
 $disabled = "";
 
 foreach ($result as  $datos) {
-    if( $datos["confirm"] == 'F' && $datos["in_transport"] == 'F'){
-        echo '<tr class="fondo03">';
-    }else if( $datos["confirm"] == 'T' && $datos["in_transport"] == 'T'){
-        echo '<tr class="fondo02">';
-    }else{
-        echo '<tr>';
-    }
-
-    echo '<td style="background=\'red\' !important;">' . $datos["ubicacion"] . '</td>
+    echo '<tr><td style="background=\'red\' !important;">' . $datos["ubicacion"] . '</td>
         <td>' . $datos["ficha"] . '</td>
         <td>' .  $datos["telefono"] . '</td>
         <td>' . $datos["ap_nombre"] . '</td>
         <td>' . $datos["turno"] . '</td>
         <td>' . $datos["horario"] . '</td>
         <td>' . $datos["concepto"] . '</td>
-        <td>' . $datos["hora_entrada"] . '</td>
-        <td>';
+        <td>' . $datos["hora_entrada"] . '</td>';
         if( $datos["confirm"] == 'T'){
-            echo $datos["fec_confirm"];
+            echo '<td class="fondo02">'.$datos["fec_confirm"];
         }else{
-            echo 'Sin confirmar';
+            echo '<td class="fondo03">Sin confirmar';
         }
-        echo '</td><td>';
+        echo '</td>';
         if( $datos["in_transport"] == 'T'){
-            echo $datos["fec_in_transport"];
+            echo '<td class="fondo02">'.$datos["fec_in_transport"];
         }else{
-            echo 'Sin confirmar';
+            echo '<td class="fondo03">Sin confirmar';
         }
         echo '</td>';
 }
