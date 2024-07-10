48ec5c1ba175f90ac2798bc1dc6d14aa73d468ac
rakodetech <ciriloramonlinaresurbina@gmail.com>
2024-07-10 16:20:30 -0400

    Actualizacion de Marcaje con documentos
    Marcajes con documentos

--- packages/planif/planif_marcaje/views/Add_actividades.php
+++ packages/planif/planif_marcaje/views/Add_actividades.php
@@ -9,7 +9,7 @@ $cliente     = $_POST['cliente'];
 $ubicacion     = $_POST['ubicacion'];
 $result  =  $marcaje->get_actividades($ficha, $cliente, $ubicacion);
 $disabled = "";
-
+	
 foreach ($result as  $datos) {
     if ($datos["realizado"] == 'SI') {
         echo '<tr class="marcar">';
@@ -35,13 +35,14 @@ foreach ($result as  $datos) {
             echo '<td>N/A</td></tr>';
         }
     } else {
-        echo '<td><img src="imagenes/ok3.gif" ' . $disabled . ' onclick="setRealizado(\'' . $datos['codigo'] . '\')"
-        alt="Realizado" title="Marcar como realizado" width="20px" height="20px" border="null"/></a></td>
-        <td><img class="imgLink" id="m_observaciones" src="imagenes/detalle.bmp" alt="Modificar Observaciones" title="Modificar Observaciones" onclick="openModalObservaciones(' . $datos["codigo"] . ')" width="15px" height="15px">(' . $datos["observaciones"] . ')</td>';
+        
+        echo '<td><img class="imgLink" id="m_observaciones" src="imagenes/nuevo.bmp" alt="Modificar Observaciones" title="Modificar Observaciones" onclick="openModalObservacionesdos(' . $datos["codigo"] . ',' .$ficha . ',' . $ci . ')" width="15px" height="15px"></td>
+         <td><img class="imgLink" id="m_observaciones" src="imagenes/detalle.bmp" alt="Modificar Observaciones" title="Modificar Observaciones" onclick="openModalObservaciones(' . $datos["codigo"] . ')" width="15px" height="15px">(' . $datos["observaciones"] . ')</td>';
         if ($datos["participantes"] == 'T') {
             echo '<td><img class="imgLink" id="m_participantes" src="imagenes/detalle.bmp" alt="Modificar Participantes" title="Modificar Participantes" onclick="openModalParticipantes(' . $datos["codigo"] . ')" width="15px" height="15px">(' . $datos["fichas"] . ')</td></tr>';
         } else {
             echo '<td>N/A</td></tr>';
         }
     }
+    
 }
