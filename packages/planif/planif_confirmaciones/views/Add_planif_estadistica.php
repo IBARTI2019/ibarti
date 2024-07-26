1f2fd8815df0485f154cfb54199351153a75c451
YAVILES <yonathanaviles123@gmail.com>
2024-07-26 11:11:16 -0400

    Update Add_planif_estadistica.php

--- packages/planif/planif_confirmaciones/views/Add_planif_estadistica.php
+++ packages/planif/planif_confirmaciones/views/Add_planif_estadistica.php
@@ -9,7 +9,7 @@ $cliente     = $_POST['cliente'];
 $ubicacion     = $_POST['ubicacion'];
 $horario     = $_POST['horario'];
 $data  =  $confirmaciones->get_estadistica($ficha, $cliente, $ubicacion, $horario);
-$porcentaje_asistenia = ($data["confirm"] * 100) / $data["total"];
-$porcentaje_transporte = ($data["in_transport"] * 100) / $data["total"];
+$porcentaje_asistenia = round((($data["confirm"] * 100) / $data["total"]), 2);
+$porcentaje_transporte = round((($data["in_transport"] * 100) / $data["total"]), 2);
 echo '<b> Confirmaciones de asistencia: '.$data["confirm"].'/'.$data["total"].' '. $porcentaje_asistenia .'% </b><br> 
 <b> Confirmaciones de transporte: '.$data["in_transport"].'/'.$data["total"].' '. $porcentaje_transporte .'% </b>';
