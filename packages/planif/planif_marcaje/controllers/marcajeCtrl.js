62711e4b5e114c7c58d7a5d416725ed35482e345
YAVILES <39055827+YAVILES@users.noreply.github.com>
2024-11-20 22:27:54 -0400

    Update marcajeCtrl.js

--- packages/planif/planif_marcaje/controllers/marcajeCtrl.js
+++ packages/planif/planif_marcaje/controllers/marcajeCtrl.js
@@ -163,7 +163,7 @@ function uploadActulizarS3marcaje(url,cod,archi,xusuario) {
             if (ubi.checked){
                 message = $("<span class='enviando email, por favor espere...</span>");
                 showMessage(message)
-                enviaremail(cod_cliente,cod_ubicacion);
+                // enviaremail(cod_cliente,cod_ubicacion);
             }
             $("#imagen").val("");
             Add_filtroX();
