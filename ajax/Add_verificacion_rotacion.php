c4737b0b2dd334475ad81c29ccbd5446d0db24a0
YAVILES <39055827+YAVILES@users.noreply.github.com>
2025-01-15 01:37:29 -0400

    Update Add_verificacion_rotacion.php

--- ajax/Add_verificacion_rotacion.php
+++ ajax/Add_verificacion_rotacion.php
@@ -195,7 +195,7 @@ if ($quincena == "01"){
 				) r ON asistencia_quincenal01.cod_ficha = r.cod_ficha
 				$where01
 			ORDER BY 1 ASC";
-	echo $sql;
+	// echo $sql;
 
 		echo "<table width='100%' border='0' align='center' class='tabla_sistema'>
 			<tr><th>".$leng['ficha']." </th><th> ".$leng['ci']."  </th><th> Nombres  </th><th> ".$leng['rol']."  </th>
