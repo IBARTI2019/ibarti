c19622d86592b839d78b209451a627ab0eb8e693
YAVILES <39055827+YAVILES@users.noreply.github.com>
2025-01-17 21:20:55 -0400

    Update Add_rop_planif_cl_vs_as_horario.php

--- ajax_rp/Add_rop_planif_cl_vs_as_horario.php
+++ ajax_rp/Add_rop_planif_cl_vs_as_horario.php
@@ -58,7 +58,7 @@ $sql = "SELECT v_as_planif_horario.fec_diaria fecha,v_as_planif_horario.cod_ubic
 FROM v_as_planif_horario,  clientes_ubicacion , clientes , estados, horarios, control
 $WHERE_21
 $WHERE_22";
-
+$result["sql_fa"] = $sql;
 $qry  = $bd->consultar($sql);
 while($rows=$bd->obtener_name($qry)){
 	$result['asistencia'][] = $rows;
@@ -90,12 +90,12 @@ INNER JOIN estados ON cu.cod_estado = estados.codigo
 $WHERE
 GROUP BY a.cod_cliente, a.cod_ubicacion, h.codigo, a.fecha
 ORDER BY 1,4";
-echo $sql;
+
 $query = $bd->consultar($sql);
 while($rows=$bd->obtener_name($query)){
 	$result['contrato'][] = $rows;
 }
-
+$result["sql_contrato"] = $sql;
 print_r(json_encode($result));
 return json_encode($result);
 
