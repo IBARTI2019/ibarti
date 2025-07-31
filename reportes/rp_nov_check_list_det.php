2e1015e7d2fdb631948e1c4552dc0dc3730550e8
YAVILES <39055827+YAVILES@users.noreply.github.com>
2025-07-31 11:07:21 -0400

    Update rp_nov_check_list_det.php

--- reportes/rp_nov_check_list_det.php
+++ reportes/rp_nov_check_list_det.php
@@ -102,8 +102,8 @@ if (isset($reporte)) {
 					     nov_check_list_det.observacion, nov_check_list.fec_us_mod,
 						 nov_status.descripcion AS nov_status,
 					   IF(ISNULL(nov_agrupacion.descripcion),'N/A',nov_agrupacion.descripcion) agrupacion, CONCAT(ft.apellidos, ' ', ft.nombres) AS trabajador
-                    FROM nov_check_list , nov_check_list_det LEFT JOIN nov_agrupacion ON nov_check_list_det.cod_nov_agrupacion = nov_agrupacion.codigo
-					 LEFT JOIN ficha ft ON nov_check_list.cod_ficha_trab = ft.codigo, 
+                    FROM nov_check_list LEFT JOIN ficha ft ON nov_check_list.cod_ficha_trab = ft.cod_ficha, 
+						 nov_check_list_det LEFT JOIN nov_agrupacion ON nov_check_list_det.cod_nov_agrupacion = nov_agrupacion.codigo, 
 						 novedades, nov_clasif , nov_tipo, nov_valores, clientes ,
 					 	 clientes_ubicacion , ficha , nov_status
                   $where
@@ -116,8 +116,8 @@ if (isset($reporte)) {
 					  nov_check_list.repuesta,
 				Sum(nov_check_list_det.valor) AS check_list_valor, Sum(nov_check_list_det.valor_max) AS valor_max,
                 nov_check_list.fec_us_mod, nov_status.descripcion AS nov_status, CONCAT(ft.apellidos, ' ', ft.nombres) AS trabajador
-           FROM nov_check_list , nov_check_list_det LEFT JOIN nov_agrupacion ON nov_check_list_det.cod_nov_agrupacion = nov_agrupacion.codigo
-		   		LEFT JOIN ficha ft ON nov_check_list.cod_ficha_trab = ft.codigo, 
+           FROM nov_check_list LEFT JOIN ficha ft ON nov_check_list.cod_ficha_trab = ft.cod_ficha, 
+		     	nov_check_list_det LEFT JOIN nov_agrupacion ON nov_check_list_det.cod_nov_agrupacion = nov_agrupacion.codigo, 
 		   		nov_clasif ,  nov_tipo, clientes , clientes_ubicacion , ficha , nov_status
         $where2
        GROUP BY nov_check_list.codigo
