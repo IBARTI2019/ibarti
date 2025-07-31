1bfe66ecd07e7009faa9097b45002a849fd84e8f
YAVILES <39055827+YAVILES@users.noreply.github.com>
2025-07-31 11:04:36 -0400

    Update rp_nov_check_list_det.php

--- reportes/rp_nov_check_list_det.php
+++ reportes/rp_nov_check_list_det.php
@@ -103,7 +103,7 @@ if (isset($reporte)) {
 						 nov_status.descripcion AS nov_status,
 					   IF(ISNULL(nov_agrupacion.descripcion),'N/A',nov_agrupacion.descripcion) agrupacion, CONCAT(ft.apellidos, ' ', ft.nombres) AS trabajador
                     FROM nov_check_list , nov_check_list_det LEFT JOIN nov_agrupacion ON nov_check_list_det.cod_nov_agrupacion = nov_agrupacion.codigo
-					 LEFT JOIN ficha ft ON nov_check_list_det.cod_ficha_trab = ft.codigo, 
+					 LEFT JOIN ficha ft ON nov_check_list.cod_ficha_trab = ft.codigo, 
 						 novedades, nov_clasif , nov_tipo, nov_valores, clientes ,
 					 	 clientes_ubicacion , ficha , nov_status
                   $where
@@ -117,7 +117,7 @@ if (isset($reporte)) {
 				Sum(nov_check_list_det.valor) AS check_list_valor, Sum(nov_check_list_det.valor_max) AS valor_max,
                 nov_check_list.fec_us_mod, nov_status.descripcion AS nov_status, CONCAT(ft.apellidos, ' ', ft.nombres) AS trabajador
            FROM nov_check_list , nov_check_list_det LEFT JOIN nov_agrupacion ON nov_check_list_det.cod_nov_agrupacion = nov_agrupacion.codigo
-		   		LEFT JOIN ficha ft ON nov_check_list_det.cod_ficha_trab = ft.codigo, 
+		   		LEFT JOIN ficha ft ON nov_check_list.cod_ficha_trab = ft.codigo, 
 		   		nov_clasif ,  nov_tipo, clientes , clientes_ubicacion , ficha , nov_status
         $where2
        GROUP BY nov_check_list.codigo
