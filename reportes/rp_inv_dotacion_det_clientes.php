915fc886738c1af64dd9a7e6c8acc44f9a5f2938
YAVILES <yonathanaviles123@gmail.com>
2026-06-02 16:29:51 -0400

    EAND en reportes de ajustes de inventario

--- reportes/rp_inv_dotacion_det_clientes.php
+++ reportes/rp_inv_dotacion_det_clientes.php
@@ -80,7 +80,11 @@ if(isset($reporte)){
                  ajuste_alcance_reng.aplicar as neto,
                  ajuste_alcance.anulado,
                  
-                 prod_sub_lineas.descripcion AS sub_linea, productos.descripcion AS producto,ajuste_alcance_reng.cantidad
+                 prod_sub_lineas.descripcion AS sub_linea, productos.descripcion AS producto,ajuste_alcance_reng.cantidad,
+                 (SELECT GROUP_CONCAT(e.cod_ean SEPARATOR ', ') 
+                  FROM ajuste_alcance_reng_eans e 
+                  JOIN ajuste_alcance_reng r ON e.cod_ajuste = r.cod_ajuste AND e.reng_num = r.reng_num 
+                  WHERE e.cod_ajuste = ajuste_alcance.codigo AND r.cod_producto = ajuste_alcance_reng.cod_producto) AS eans
             FROM ajuste_alcance , ajuste_alcance_reng,clientes,clientes_ubicacion,productos , prod_lineas ,prod_sub_lineas 
         $where GROUP BY ajuste_alcance.codigo,ajuste_alcance_reng.cod_producto
 HAVING MAX(ajuste_alcance.codigo)
@@ -95,16 +99,16 @@ ORDER BY 2 ASC";
 
 		$query01  = $bd->consultar($sql);
 		 echo "<table border=1>";
- 	 echo "<tr><th> Código </th><th> Fecha </th><th>Cliente</th><th>".$leng['ubicacion']."
-			   <th> Linea </th><th> Sub Linea </th><th> Cod. Producto </th><th> Producto </th><th> Cantidad </th>";
+ 	 echo "<tr><th> Código </th><th> Fecha </th><th>Cliente</th><th>".$leng['ubicacion']."</th>
+			   <th> Linea </th><th> Sub Linea </th><th> Cod. Producto </th><th> Producto </th><th> EANs </th><th> Cantidad </th>";
 			   echo ($restri=="F")?'<th class="etiqueta">Tipo</th>':'';
 		echo "<th> Anulado</th></tr>";
 
 		while ($row01 = $bd->obtener_num($query01)){
-		 echo "<tr><td> ".$row01[0]." </td><td>".$row01[1]."</td><td>".$row01[2]."</td><td>".$row01[3]."</td><td>".$row01[5]."</td><td>".$row01[9]."</td>
-		 <td>".$row01[4]."</td><td>".$row01[9]."</td><td>".$row01[10]."</td <td>".$row01[6]."";
-				   echo ($restri=="F")?'<td class="texto">'.$row01[7].'</td>':''; 
-				    
+		 echo "<tr><td> ".$row01[0]." </td><td>".$row01[1]."</td><td>".$row01[2]."</td><td>".$row01[3]."</td><td>".$row01[5]."</td><td>".$row01[8]."</td>
+		 <td>".$row01[4]."</td><td>".$row01[9]."</td><td>".$row01[11]."</td><td>".$row01[10]."</td>";
+				   echo ($restri=="F")?'<td class="texto">'.$row01[6].'</td>':''; 
+				   echo "<td>".$row01[7]."</td></tr>";
 		}
 		 echo "</table>";
 	}
@@ -124,14 +128,15 @@ ORDER BY 2 ASC";
         <table>
 		<tbody>
             <tr style='background-color: #4CAF50;'>
-            <th width='10%'  style='text-align:center;'>Código</th>
-            <th width='20%'>Fecha</th>
-            <th width='25%'>".$leng['ubicacion']."</th>
-            <th width='15%'>Linea</th>
-            <th width='15%'>Sub Linea</th>
+            <th width='10%' style='text-align:center;'>Código</th>
+            <th width='15%'>Fecha</th>
+            <th width='20%'>".$leng['ubicacion']."</th>
+            <th width='10%'>Linea</th>
+            <th width='10%'>Sub Linea</th>
             <th width='15%'>Producto</th>
-            <th width='10%'  style='text-align:center;'>Cantidad</th>
-            <th width='10%'  style='text-align:center;'>Tipo</th>
+            <th width='10%'>EANs</th>
+            <th width='5%' style='text-align:center;'>Cant.</th>
+            <th width='5%' style='text-align:center;'>Tipo</th>
             </tr>";
 
             $f=0;
@@ -139,17 +144,18 @@ ORDER BY 2 ASC";
     	 if ($f%2==0){
                 echo "<tr>";
             }else{
-                echo "<tr class='class= odd_row'>";
+                echo "<tr class='odd_row'>";
             }
    echo   "<td width='10%' style='text-align:center;'>".$row[0]."</td>
-            <td width='20%'>".$row[1]."</td>
-            <td width='10%'>".$row[2]."</td>
-            <td width='10%'>".$row[4]."</td>
-            <td width='10%'>".$row[7]."</td>
-             <td width='10%'>".$row[8]."</td>
-             <td width='10%'>".$row[9]."</td>
-             <td width='10%'>".$row[5]."</td>
-            ";
+            <td width='15%'>".$row[1]."</td>
+            <td width='20%'>".$row[3]."</td>
+            <td width='10%'>".$row[5]."</td>
+            <td width='10%'>".$row[8]."</td>
+             <td width='15%'>".$row[9]."</td>
+             <td width='10%'>".$row[11]."</td>
+             <td width='5%' style='text-align:center;'>".$row[10]."</td>
+             <td width='5%' style='text-align:center;'>".$row[6]."</td>
+            </tr>";
             $f++;
          }
 
